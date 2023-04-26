edf9c88bc352414c43d1f2d388d1be1763f29dc5
sabir <sabirovich2014@mail.ru>
2023-04-26 19:21:45 +0400

    first

--- src/capitalize.js
+++
@@ -1,7 +0,0 @@
-const capitalize = (word) => {
-    const first = word[0].toUpperCase();
-    const two = word.slice(1);
-    return `${first}${two}`;
-};
-
-export default capitalize;
--- tests/capitalize.test.js
+++
@@ -1,9 +0,0 @@
-import capitalize from '../src/capitalize.js';
-
-import { strict as assert } from 'assert';
-
-  assert.strictEqual('hello', 'hello');
-
-  assert.equal(capitalize('hello'), 'Hello');
-  
-  console.log('Все тесты пройдены!');
